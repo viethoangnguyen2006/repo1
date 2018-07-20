cbfe4681b8eeaf99970b81927a823c16e3eb3f1c
NGUYEN <vhnguyen@gmail.com>
2018-07-20 11:34:20 +0200

    Version 1 de mon application console

---
+++ GitRepo2/GitRepo2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitRepo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            
+        {
+
+            Console.WriteLine("Demo avec Git repo2 Version1");
+            Console.Read();
+        }
+    }
+}
